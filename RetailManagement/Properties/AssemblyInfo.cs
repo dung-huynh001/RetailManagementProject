4d9150b5fc62c34f643c21035d025022ec397525
dung-huynh001 <dungb1906640@gmail.com>
2023-03-23 20:50:39 +0700

    Version 1.0

--- RetailManagement/Properties/AssemblyInfo.cs
+++ RetailManagement/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CT239_NienLuan_QLBH")]
+[assembly: AssemblyTitle("RetailManagement")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CT239_NienLuan_QLBH")]
+[assembly: AssemblyProduct("RetailManagement")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
